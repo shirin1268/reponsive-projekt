18f5ed94276fe1d10f8d57f5b0c9ac92ce345f2e
Shirin Vazirian <Shirin1268@gmail.com>
2016-03-10 10:53:35 +0100

    no message

---
+++ .idea/main.js
@@ -0,0 +1,11 @@
+/**
+ * Created by shirin1268 on 10-03-2016.
+ */
+
+$(funktion(){
+    $('ul li a').on('click',funktion()
+    {
+        $(this).parent().addclass('current').sibling().removeclass('current');
+    }
+);
+});
